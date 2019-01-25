3a50c647dab756d5e2d324c13e15839f26e1b822
Pietro Saccardi <lizardm4@gmail.com>
2019-01-25 20:14:32 +0100

    Missing quotes

--- src/scripts/cpp-armv6-linux-gnueabihf.sh
+++ src/scripts/cpp-armv6-linux-gnueabihf.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-c++ --target=arm-linux-gnueabihf -march=armv6 -mfloat-abi=hard -mfpu=vfp --sysroot /root/sysroot -fuse-ld=lld $@
+c++ --target=arm-linux-gnueabihf -march=armv6 -mfloat-abi=hard -mfpu=vfp --sysroot /root/sysroot -fuse-ld=lld "$@"
