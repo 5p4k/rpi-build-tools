d9b93832ff44d4b37980e424f42cc901ed774593
Pietro Saccardi <lizardm4@gmail.com>
2019-01-26 03:09:39 +0100

    Added static libc++ toolchain

---
+++ src/cmake_toolchains/RpiStaticLibCxxToolchain.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_LIST_DIR}/RPiLibCxxToolchain.cmake)
+
+# Change the linking flags:
+#  -nostdlib++ will prevent linking against libc++.so
+#  -l:libc++(abi).a will link statically against libc++
+#  -lpthread is needed because it's used by the standard library
+set(CMAKE_CXX_STANDARD_LIBRARIES "-l:libc++.a -l:libc++abi.a -lpthread -nostdlib++")
